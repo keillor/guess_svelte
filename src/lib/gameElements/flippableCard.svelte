bf4349168186f3ac9af39b0186f2cb0249c48975
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-09-11 00:21:12 -0700

    patched previously clicked button does not loose focus.

--- src/lib/gameElements/flippableCard.svelte
+++ src/lib/gameElements/flippableCard.svelte
@@ -36,7 +36,7 @@
 		</span>
 	</div>
 	<div class="back">
-		<svg width="400" height="400" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
+		<svg width="100" height="400" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
 			<path
 				id="path2"
 				fill="var(--color-purple-800)"

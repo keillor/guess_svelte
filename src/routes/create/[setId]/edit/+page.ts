003c59d0a8694d22c3c524cc9116a4e9ec13ec61
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-10-04 18:01:44 -0700

    403 permissions (edit) not triggering. removed.

--- src/routes/create/[setId]/edit/+page.ts
+++ src/routes/create/[setId]/edit/+page.ts
@@ -1,8 +1,6 @@
 import { goto } from '$app/navigation';
 import { user } from '$lib/db/auth.svelte.js';
-import ToastError from '$lib/gameElements/ToastError.svelte';
 import { CharacterSet } from '$lib/models/CharacterSet.svelte.js';
-import { toast } from 'svelte-sonner';
 
 export async function load({params}) {
     const set = await CharacterSet.fromFirebase(params.setId);
@@ -10,13 +8,8 @@ export async function load({params}) {
         goto('/404');
     }
     if(user.user.uid != set.userId) {
+        //if you are not the owner, redirect to the view page.
         goto(`/set/${params.setId}`);
-        toast.custom(ToastError, {
-            componentProps: {
-                loading: false,
-                message: 'You do not have permission to edit this set.'
-            }
-        })
     }
     return {
         set: set,

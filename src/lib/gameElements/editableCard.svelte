85fff615fa944ece1c033e6063accfad523ebcfe
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-10-04 18:20:26 -0700

    removed unused tailwind from svg

--- src/lib/gameElements/editableCard.svelte
+++ src/lib/gameElements/editableCard.svelte
@@ -86,7 +86,7 @@
 					viewBox="0 0 24 24"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
-					class="absolute z-10 bg-gray-200/70 fill-blue-500 characterImage transition-all"
+					class="absolute z-10 bg-gray-200/70 fill-blue-500 characterImage"
 				>
 					<path
 						d="M11 14.9861C11 15.5384 11.4477 15.9861 12 15.9861C12.5523 15.9861 13 15.5384 13 14.9861V7.82831L16.2428 11.0711L17.657 9.65685L12.0001 4L6.34326 9.65685L7.75748 11.0711L11 7.82854V14.9861Z"

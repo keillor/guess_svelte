99a1be6b4b4a7bee031ad52449458e58d178fdb5
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-10-27 20:19:32 -0700

    load join

--- src/routes/join/[gameId]/+page.ts
+++ src/routes/join/[gameId]/+page.ts
@@ -1,4 +1,5 @@
 import { goto } from '$app/navigation';
+import { user } from '$lib/db/auth.svelte.js';
 import { GuessWhoGame } from '$lib/guessWho.svelte'
 import { CharacterSet } from '$lib/models/CharacterSet.svelte';
 
@@ -6,6 +7,15 @@ export async function load({ params }) {
     try {
         const game = await GuessWhoGame.loadFromFirestore(params.gameId);
         const cards = await CharacterSet.fromFirebase(game?.characterSetId);
+        if(game?.players.includes(user.user.uid)) {
+            if(game?.players.length == 1) {
+                // player who created game
+                goto(`/wait/${params.gameId}`);
+            } else {
+                // both players are present
+                goto(`/play/${params.gameId}`);
+            }
+        }
         return {
             game,
             cards

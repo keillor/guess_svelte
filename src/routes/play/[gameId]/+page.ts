e76916797414358370393ec0ca559c631b57f703
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-10-27 20:19:21 -0700

    load into specific game under `/play/<gameId>`.

---
+++ src/routes/play/[gameId]/+page.ts
@@ -0,0 +1,33 @@
+import { goto } from '$app/navigation';
+import { user } from '$lib/db/auth.svelte';
+import { GuessWhoGame } from '$lib/guessWho.svelte.js';
+import { CharacterSet } from '$lib/models/CharacterSet.svelte.js';
+
+export async function load({ params }) {
+    try {
+        const game = await GuessWhoGame.loadFromFirestore(params.gameId);
+        if(game == null) {
+            console.info('game does not exist')
+            goto('/404');
+            return;
+        }
+        const cards = await CharacterSet.fromFirebase(game?.characterSetId);
+        if(game == null) {
+            console.info('card does not exist')
+            goto('/404');
+            return;
+        }
+        if(!game.players.includes(user.user?.uid)) {
+            console.info('player not in game')
+            goto('/404');
+        }
+        return {
+            game,
+            cards
+        }
+    } catch (e) {
+        console.error('whoops! looks like there was an error...')
+        goto('/');
+        return;
+    }
+}

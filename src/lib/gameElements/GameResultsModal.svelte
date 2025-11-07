07356d102fafa27c147cd1769db58e0d77ffc8a2
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-11-06 22:05:06 -0800

    new file for showing win / loss status

---
+++ src/lib/gameElements/GameResultsModal.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+ import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
+ import { buttonVariants } from "$lib/components/ui/button/index.js";
+ let { isOpen = $bindable(), playerWon = $bindable() } = $props();
+</script>
+ 
+<AlertDialog.Root bind:open={isOpen}>
+ <AlertDialog.Trigger class={buttonVariants({ variant: "outline" })}>
+  Show Dialog
+ </AlertDialog.Trigger>
+ <AlertDialog.Content>
+  <AlertDialog.Header>
+   <AlertDialog.Title>The results are in...</AlertDialog.Title>
+   <AlertDialog.Description>
+    {#if playerWon}
+    You win!
+    {:else}
+    you lose!
+    {/if}
+   </AlertDialog.Description>
+  </AlertDialog.Header>
+  <AlertDialog.Footer>
+   <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+   <AlertDialog.Action>Continue</AlertDialog.Action>
+  </AlertDialog.Footer>
+ </AlertDialog.Content>
+</AlertDialog.Root>
+
+
+<!-- TODO: implement WINNER / LOSER animation -->

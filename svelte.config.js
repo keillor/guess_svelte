7d5e4c669f441a032092e8c91ffb19d0e590a2c4
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-11-19 23:38:41 -0800

    gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,15 +7,19 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ 
-		fallback: 'fallback.html'
-	}),
+	kit: {
+		adapter: adapter({
+			fallback: 'fallback.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
 		prerender: {
 			handleUnseenRoutes: 'warn',
 			crawl: true,
 			handleMissingId: 'warn'
 		}
-	 }
+	}
 };
 
 export default config;
